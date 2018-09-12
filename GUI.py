a545d9c71687e787fd4442ae52cf5dbd35200a00
Minh Hua <42155133+duyminh1998@users.noreply.github.com>
2018-09-11 20:30:07 -0700

    GUI in development

---
+++ GUI.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import pandas as pd
+import datetime
+'''This is a WIP fdevelopment of a GUI for the funko_price_scraper.py script'''
+
+class PriceGenerator:
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.search_label = Label(frame, text="Please enter product to search:")
+        self.search_label.pack(side=LEFT)
+
+        self.search = Entry(frame)
+        self.search.pack(side=LEFT)
+
+        self.search_enter = Button(frame, text="Search!", command=self.pop_search)
+        self.search_enter.pack(side=RIGHT)
+
+    def pop_search(self):
+        now = datetime.datetime.now()
+        df = pd.read_csv("pop_prices_csv_" + str(now.month) + "_" + str(now.day) + ".csv", error_bad_lines=False)
+        pop_graph_df = pd.DataFrame(columns = ['NAME', 'STORE', 'ORIGINAL PRICE', 'SALE PRICE'])
+        pop_name = self.search.get().lower()
+        j = 0
+        for i in range(len(df)):
+            if pop_name in df.loc[i, "NAME"].lower():
+                pop_graph_df.loc[j] = df.loc[i]
+                j += 1
+        results = Label(text=pop_graph_df.drop_duplicates().to_string())
+        results.pack()
+
+root = Tk()
+main = PriceGenerator(root)
+root.mainloop()
